cc59e8e0a483c98a6fcac3ab65dc868cf3b1b226
shaibu <shaibusunnuma@gmail.com>
2021-03-19 02:06:42 +0000

    completed reviews project

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Review from './Review';
+import reviews from './data'
+
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  const [reviews, setReviews] = useState([]);
+
+  return <main>
+    <section className='container'>
+      <div className='title'>
+        <h2>our reviews</h2>
+        <div className='underline'></div>
+      </div>
+      <Review/>
+    </section>
+    
+  </main>;
 }
 
 export default App;
--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -3,7 +3,59 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index]
+
+  const prevPerson = () =>{
+    setIndex((index)=>{
+      index = index-1;
+      if(index < 0){
+        index = people.length-1;
+      }
+      return index;
+    }); 
+    
+  }
+
+  const nextPerson = () =>{
+    setIndex((index)=>{
+      index = index+1;
+      if(index > people.length-1){
+        index = 0;
+      }
+      return index;
+    });
+  }
+
+  const randomPerson = () =>{
+    let newIndex = Math.floor(Math.random()*people.length);
+    while(newIndex === index)
+      newIndex = Math.floor(Math.random()*people.length);
+    
+    setIndex((index)=>{
+      return newIndex;
+    })
+  }
+
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img'/>
+        <span className='quote-icon'>
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}><FaChevronLeft/></button>
+        <button className='next-btn' onClick={nextPerson}><FaChevronRight/></button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>suprise me</button>
+    </article>
+  );
 };
 
 export default Review;
